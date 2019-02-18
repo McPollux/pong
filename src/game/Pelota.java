4b4e3bf354ed724bf070fb50c58cb511b276e14d
FerMod <tudelaigarcia@gmail.com>
2019-02-18 22:11:22 +0100

    Simplify if blocks into one sentence

--- src/game/Pelota.java
+++ src/game/Pelota.java
@@ -62,16 +62,8 @@ public class Pelota {
 		double d2x = p1.getX() - (p2.getX() + p2.getWidth() + slack);	// d2x = a->min.x - b->max.x;
 		double d2y = p1.getY() - (p2.getY() + p2.getWidth() + slack);	// d2y = a->min.y - b->max.y;
 
-		if (d1x > 0.0f || d1y > 0.0f) {
-			return false;
-		} 
-
-		if (d2x > 0.0f || d2y > 0.0f) {
-			return false;
-		}
-
-		return true;
-	}
+        return !(d1x > 0.0f) && !(d1y > 0.0f) && !(d2x > 0.0f) && !(d2y > 0.0f);
+    }
 
 	public void draw(Graphics g) {
 		g.setColor(Color.WHITE);
